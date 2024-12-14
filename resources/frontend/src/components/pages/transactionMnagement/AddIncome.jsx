f27752dfa50a97d9c8982a52b398e383dccd0d72
Kavishka <kavishkaimalsha@gmail.com>
2024-12-15 01:04:41 +0530

    Refactoring common components in the modal and pages

--- resources/frontend/src/components/pages/transactionMnagement/AddIncome.jsx
+++ resources/frontend/src/components/pages/transactionMnagement/AddIncome.jsx
@@ -1,8 +1,10 @@
 import incomeIcon from "../../../assets/images/income.png";
 import {useState} from "react";
-import FormSubmitBtn from "./commonFormFields/FormSubmitBtn.jsx";
-import FormInputField from "./commonFormFields/FormInputField.jsx";
-import FormSelectInput from "./commonFormFields/FormSelectInput.jsx";
+import FormSubmitBtn from "./commonFields/FormSubmitBtn.jsx";
+import FormInputField from "./commonFields/FormInputField.jsx";
+import FormSelectInput from "./commonFields/FormSelectInput.jsx";
+import ModalCloseBtn from "./commonFields/ModalCloseBtn.jsx";
+import AddBtn from "./commonFields/AddBtn.jsx";
 
 const AddIncome = () => {
     const [isModelVisible, setIsModelVisible] = useState(false)
@@ -23,13 +25,7 @@ const AddIncome = () => {
                         <h1 className="font-bold text-2xl mx-1">Income Management</h1>
                     </div>
                     <div>
-                        <button
-                            onClick={() => {showModel(isModelVisible)}}
-                                className="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                type="button">
-                            Add Income
-                        </button>
-
+                        <AddBtn btnName="Add Income" onClick={() => {showModel(false)}}/>
                         {isModelVisible && (
                             <div id="crud-modal" tabIndex="-1" aria-hidden="true"
                                  className="flex justify-center backdrop-blur-sm overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -40,20 +36,7 @@ const AddIncome = () => {
                                             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                                                 Add Income Details
                                             </h3>
-                                            <button type="button"
-                                                    onClick={() => {
-                                                        showModel(isModelVisible)
-                                                    }}
-                                                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                                    data-modal-toggle="crud-modal">
-                                                <svg className="w-3 h-3" aria-hidden="true"
-                                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                    <path stroke="currentColor" stroke-linecap="round"
-                                                          stroke-linejoin="round" stroke-width="2"
-                                                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                                </svg>
-                                                <span className="sr-only">Close modal</span>
-                                            </button>
+                                            <ModalCloseBtn onClose={() => {showModel(true)}}/>
                                         </div>
                                         <form className="p-4 md:p-5">
                                             <div className="grid gap-4 mb-4 grid-cols-2">
