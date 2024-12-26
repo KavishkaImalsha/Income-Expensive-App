9c189d5109e784e3ccb4438d9ebe97c9c6aa153a
Kavishka <kavishkaimalsha@gmail.com>
2024-12-26 09:42:08 +0530

    Make migration, model and factory for expenses

---
+++ app/Models/Expense.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class Expense extends Model
+{
+    use HasFactory;
+    protected $table = 'expenses';
+
+    protected $fillable = [
+      'expense_amount',
+      'expense_category'
+    ];
+}
