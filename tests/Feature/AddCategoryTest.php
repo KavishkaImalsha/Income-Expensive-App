165e57ba15580be30986e7698fb042c28f2c1a0a
Kavishka <kavishkaimalsha@gmail.com>
2024-12-26 19:58:54 +0530

    Implement test case for add category without passing any data to database

--- tests/Feature/AddCategoryTest.php
+++ tests/Feature/AddCategoryTest.php
@@ -49,4 +49,21 @@ class AddCategoryTest extends TestCase
         ]);
         $this->assertDatabaseMissing('categories', $category);
     }
+
+    public function test_add_category_without_passing_any_data()
+    {
+        $category = Category::factory()->make([
+            'category_name' => null,
+            'category_type' => null
+            ])->toArray();
+
+        $response = $this->postJson('api/add-category', $category);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors([
+            'category_name',
+            'category_type'
+        ]);
+        $this->assertDatabaseMissing('categories', $category);
+    }
 }
